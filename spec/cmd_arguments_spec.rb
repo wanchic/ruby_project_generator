463643b02997d7608ab2fe021bf6c8e2082ca6bb
Eric Wanchic <eric@gratiel.com>
2019-05-12 20:52:58 -0500

    Test for message, 100% coverage.

--- spec/cmd_arguments_spec.rb
+++ spec/cmd_arguments_spec.rb
@@ -1,3 +1,4 @@
+require 'active_support/core_ext/kernel/reporting'
 require 'cmd_arguments'
 
 def rand_name
@@ -12,9 +13,18 @@ describe CmdArguments do
   end
 
   it "should be empty if two or more command arguments are given" do
-    arg = (2..((rand*10))+2).map{|| rand_name}
+    arg = (1..((rand*10))+2).map{|| rand_name}
     prog_name = CmdArguments.program_name arg
     expect(prog_name).to be(nil)
+  end
+
+  it "should return message if two or more command arguments are given" do
+    output = capture(:stdout) do
+      arg = (1..((rand*10))+2).map{|| rand_name}
+      prog_name = CmdArguments.program_name arg
+    end
+
+    expect(output).to include 'Please provide only one project name'
   end
 
   it "should be provide a name if one command argument is given" do
