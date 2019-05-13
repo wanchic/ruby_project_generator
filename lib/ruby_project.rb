31c96dd4241b8ce102522f0b3c6c79c090aa72e5
Eric Wanchic <eric@gratiel.com>
2019-05-12 21:10:55 -0500

    default to 'new_app'

--- lib/ruby_project.rb
+++ lib/ruby_project.rb
@@ -1,7 +1,7 @@
 class RubyProject
   attr_reader :project_name
 
-  def initialize(project_name)
+  def initialize(project_name = 'new_app')
     @project_name = project_name
   end
 
--- spec/ruby_project_spec.rb
+++ spec/ruby_project_spec.rb
@@ -2,11 +2,21 @@ require 'ruby_project'
 
 describe RubyProject do
   context 'Initialization' do
-    it 'should store the project_name' do
-      p_name = rand_name
-      rp = RubyProject.new(p_name)
+    context 'when the project_name is specified' do
+      it 'should store the project_name' do
+        p_name = rand_name
+        rp = RubyProject.new(p_name)
 
-      expect(rp.project_name).to eq(p_name)
+        expect(rp.project_name).to eq(p_name)
+      end
     end
+
+    context 'when the project_name is NOT specified' do
+      it 'should store the default name of \'new_app\'' do
+        rp = RubyProject.new
+        expect(rp.project_name).to eq('new_app')
+      end
+    end
+
   end
 end
